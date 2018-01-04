8aacb7dd8e0f5424ccb768e6027b0170477b951a
Patrick Heiloo <patrick@webrandcreative.nl>
2018-01-04 13:26:20 +0100

    First commit
    My first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!-- 
+	[TITLE]
+	[DESCRIPTION]
+	[URL]
+-->
+<!doctype html>
+<html lang="nl">
+	<head>
+		<meta charset="utf-8" />
+		<title>[TITLE]</title>
+		<meta name="description" content="[DESCRIPTION]">
+
+		<meta property="og:locale" content="nl_NL"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:title" content="[TITLE]"/>
+		<meta property="og:description" content="[DESCRIPTION]"/>
+		<meta property="og:url" content="[URL]"/>
+		<meta property="og:site_name" content="[TITLE]"/>
+		<meta property="og:image" content="assets/img/fb_share.jpg"/>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		
+		<link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="assets/img/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="assets/img/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="assets/img/favicons/manifest.json">
+		<link rel="mask-icon" href="assets/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="apple-mobile-web-app-title" content="[TITLE]">
+		<meta name="application-name" content="[TITLE]">
+		<meta name="theme-color" content="#ffffff">
+		
+		<link rel="stylesheet" href="assets/css/style.css" />
+		<!-- <script src="assets/js/script.js"></script> -->
+    </head>
+	<body>
+	<!-- Thanks for watching the source code - Made by We Brand Creative - www.webrandcreative.nl -->
+	
+	<!-- Thanks for watching the source code - Made by We Brand Creative - www.webrandcreative.nl -->
+	</body>
+</html>
